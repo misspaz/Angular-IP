040047c89e2d2fa1a3534cce1a9b8ed36ebac5fc
misspaz <macarenapaz91@gmail.com>
2023-12-11 14:50:59 +0100

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { ProjectDetailComponent } from './projects/project-detail/project-detail.component';
-import { HomeComponent } from './home/home.component';
-
-export const routes: Routes = [
-  { path: '', component: HomeComponent }, 
-  { path: 'project/:id', component: ProjectDetailComponent }, 
-];
